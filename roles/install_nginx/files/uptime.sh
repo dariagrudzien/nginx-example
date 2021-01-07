2e4099d24144154ffeef83c021b42aca07ceb107
Daria Grudzien <daria.grudzien@oracle.com>
2021-01-07 18:44:57 +0100

    feat: add cron jobs to generate files

---
+++ roles/install_nginx/files/uptime.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+echo "<html><head>"
+echo "<title>uptime</title>"
+echo '<meta http-equiv="Content-type"
+content="text/html;charset=UTF-8">'
+echo "</head><body>"
+uptime
+echo "</body></html>"
